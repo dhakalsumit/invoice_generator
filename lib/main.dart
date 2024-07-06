6c5e65ef943623a28a4925dce3336dc11c6a0d0b
Sumit Dhakal <dhakalsumeet123@gmail.com>
2024-07-06 12:07:36 +0545

    pdf generator

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:billing_app/screens/Invoice%20details/sender_details.dart';
+import 'package:billing_app/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Invoice',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
